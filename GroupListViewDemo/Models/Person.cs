280bdd4adb8a881b4410532571ae47d79a805bc4
李继龙 <kljzndx@outlook.com>
2019-01-24 08:27:36 +0800

    添加注释

--- GroupListViewDemo/Models/Person.cs
+++ GroupListViewDemo/Models/Person.cs
@@ -9,8 +9,17 @@
             Position = position;
         }
 
+        /// <summary>
+        /// 名字
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
         public byte Age { get; }
+        /// <summary>
+        /// 职位
+        /// </summary>
         public string Position { get; set; }
     }
 }
