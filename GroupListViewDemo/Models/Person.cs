f404c38b96dfc2db5d137a63c88512e281e2f7c5
李继龙 <kljzndx@outlook.com>
2019-01-24 08:20:19 +0800

    准备模型

---
+++ GroupListViewDemo/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace GroupListViewDemo.Models
+{
+    public class Person
+    {
+        public Person(string name, byte age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; }
+        public byte Age { get; }
+    }
+}
---
+++ GroupListViewDemo/Models/PersonGroup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupListViewDemo.Models
+{
+    public class PersonGroup
+    {
+        public PersonGroup(string name, IEnumerable<Person> people)
+        {
+            Name = name;
+            People = people.ToList();
+        }
+
+        public string Name { get; }
+        public List<Person> People { get; }
+    }
+}
