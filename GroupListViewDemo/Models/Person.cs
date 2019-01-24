2f96fb802ca47b5388b1dffe112ac165f77b3702
李继龙 <kljzndx@outlook.com>
2019-01-24 08:26:23 +0800

    添加一个职业字段

--- GroupListViewDemo/Models/Person.cs
+++ GroupListViewDemo/Models/Person.cs
@@ -2,13 +2,15 @@
 {
     public class Person
     {
-        public Person(string name, byte age)
+        public Person(string name, byte age, string position)
         {
             Name = name;
             Age = age;
+            Position = position;
         }
 
         public string Name { get; }
         public byte Age { get; }
+        public string Position { get; set; }
     }
 }
