98e354daba38d7d445b6d22b2c221cf5f1501058
李继龙 <kljzndx@outlook.com>
2019-01-24 09:25:49 +0800

    编写分组和ui代码

--- GroupListViewDemo/MainPage.xaml.cs
+++ GroupListViewDemo/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using GroupListViewDemo.Models;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -22,9 +23,52 @@ namespace GroupListViewDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<PersonGroup> _source;
+        private CollectionViewSource _dataSource;
+
         public MainPage()
         {
             this.InitializeComponent();
+            _source = new List<PersonGroup>();
+
+            List<Person> people = new List<Person>
+            {
+                new Person("Tom", 22, "Program"),
+                new Person("jarry", 22, "Manager"),
+                new Person("张三", 22, "Designer"),
+                new Person("李四", 22, "Program"),
+                new Person("王五", 22, "Designer"),
+                new Person("Tim", 22, "Program"),
+                new Person("Tom", 22, "Manager"),
+                new Person("Tom", 22, "Program"),
+                new Person("Tom", 22, "Program"),
+                new Person("Tom", 22, "Manager"),
+                new Person("Tom", 22, "Designer"),
+                new Person("Tom", 22, "Program"),
+                new Person("Tom", 22, "Designer"),
+            };
+
+            // 分组过程
+            {
+                Dictionary<string, List<Person>> allGroups = new Dictionary<string, List<Person>>();
+                foreach (var person in people)
+                {
+                    if (!allGroups.ContainsKey(person.Position))
+                        allGroups[person.Position] = new List<Person>();
+
+                    allGroups[person.Position].Add(person);
+                }
+
+                foreach (var group in allGroups)
+                    _source.Add(new PersonGroup(group.Key, group.Value));
+            }
+
+            _dataSource = new CollectionViewSource
+            {
+                IsSourceGrouped = true,
+                Source = _source,
+                ItemsPath = new PropertyPath("People")
+            };
         }
     }
 }
