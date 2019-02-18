da4c9371596139206d718819ed142743310f6973
李继龙 <kljzndx@outlook.com>
2019-02-18 10:28:08 +0800

    完成编写

--- HttpClientProgressDemo/MainPage.xaml.cs
+++ HttpClientProgressDemo/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Http;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -22,9 +24,31 @@ namespace HttpClientProgressDemo
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private readonly HttpClient _http = new HttpClient();
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void StartDownload_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            Download_ProgressBar.Value = 0;
+            if (!String.IsNullOrWhiteSpace(Url_TextBox.Text))
+                await _http.GetAsync(new Uri(Url_TextBox.Text)).AsTask(new Progress<HttpProgress>(Http_Progress));
+            Download_ProgressBar.Value = 100;
+        }
+
+        private void Http_Progress(HttpProgress obj)
+        {
+            Received_Run.Text = obj.BytesReceived.ToString();
+            if (obj.TotalBytesToReceive.HasValue)
+            {
+                ReceiveTotal_Run.Text = obj.TotalBytesToReceive.ToString();
+                double d = obj.BytesReceived * 100D / obj.TotalBytesToReceive.Value;
+                
+                Download_ProgressBar.Value = d;
+            }
         }
     }
 }
