11d7744cd4f75335c8b22e363bc87e9e6f07a5d4
李继龙 <kljzndx@outlook.com>
2021-05-13 08:35:25 +0800

    init code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button >hello world</Button>
+      </div>
+    )
+  }
+}
