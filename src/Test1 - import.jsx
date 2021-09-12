5e3d96830f0c2a60513f857ccf99415764d29be4
李继龙 <kljzndx@outlook.com>
2021-09-12 15:22:20 +0800

    准备测试代码

--- src/Test1 - import.jsx
+++ src/Test1 - import.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Hello from './Components/Hello'
 
 export default class Test1 extends Component {
   render() {
     return (
       <div>
-        
+        <Hello name='world'/>
+
+        <div>content</div>
       </div>
     )
   }
